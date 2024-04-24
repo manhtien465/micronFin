375ba6c70c0aee06362121676defad54edb17c31
tien.nguyen <nguyenmtien465@gmail.com>
2024-04-24 11:55:02 +0700

    add seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,23 @@ import Banner from './component/homepage/Banner';
 import OurService from './component/homepage/OurService';
 import Insurance from './component/homepage/Insurance';
 import Commercial from './component/homepage/Commercial';
-
-export default function Home() {
+import { getDataHomePage } from '@/app/service/homepage';
+import Head from 'next/head';
+// import SeoHomePage from './component/seo/homepage';
+// import { DefaultSeo } from 'next-seo';
+export default async function Home() {
   return (
-    <main>
-      <Header></Header>
-      <Banner/>
-      <AboutUs />
-      <OurService/>
-      <Insurance/>
-      <Commercial/>
-      <Footer></Footer>
-    </main>
+    <>
+      <main>
+        {/* <SeoHomePage></SeoHomePage> */}
+        <Header></Header>
+        <Banner />
+        <AboutUs />
+        <OurService />
+        <Insurance />
+        <Commercial />
+        <Footer></Footer>
+      </main>
+    </>
   );
 }
---
+++ src/app/service/homepage.ts
@@ -0,0 +1,6 @@
+export async function getDataHomePage() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/homepage?populate=*`
+  );
+  return res.json();
+}
