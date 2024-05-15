2f0a103a15b748f50e5b6ec83862bffa60e8ffb4
tien.nguyen <nguyenmtien465@gmail.com>
2024-05-15 12:48:31 +0700

    change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 
 module.exports = {
-    images: {
-      domains: ['eloquent-boot-c730ce64e9.media.strapiapp.com'],
-    },
+  images: {
+    domains: ['https://api.microfin.vn'],
+  },
 };
